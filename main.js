005bb2e062e6dbbb6e13400c2ce84bf8348e73c7
LivingtonMerello98 <livington.merello@gmail.com>
2024-05-28 21:45:25 +0200

    [upd] messages printed on the page

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
+'use strict'
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            title: 'Mails',
+            mails: []
+        };
+    },
+    created() {
+        console.log('chiamata axios');
+        const requests = [];
+        
+        // creo 5 richieste
+        for (let i = 0; i < 5; i++) {
+            requests.push(
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            );
+        }
+
+        // attendo tutte le richieste completate
+        Promise.all(requests)
+            .then(responses => {
+                responses.forEach(response => {
+                    this.mails.push(response.data.response);
+                });
+            })
+    }
+}).mount('#app');
 
