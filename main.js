03af0eccb33db28bb09244226e578fbe2b52a870
LivingtonMerello98 <livington.merello@gmail.com>
2024-06-03 15:37:16 +0200

    name folder changed in vuee-mail-list

--- main.js
+++ main.js
@@ -1,32 +1,40 @@
 'use strict'
 
-const { createApp } = Vue;
+console.log('sono connesso')
 
-createApp({
-    data() {
-        return {
-            title: 'Mails',
-            mails: []
-        };
-    },
-    created() {
-        console.log('chiamata axios');
-        const requests = [];
+ const { createApp } = Vue;
+
+ createApp({
+     data() {
+         return {
+             title: 'Mails',
+             mails: []
+         };
+     },
+     created() {
+         console.log('chiamata axios');
+         const requests = [];
         
-        // creo 5 richieste
-        for (let i = 0; i < 5; i++) {
-            requests.push(
-                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            );
-        }
+         // creo 5 richieste
+         for (let i = 0; i < 5; i++) {
+             requests.push(
+                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+             );
+         }
 
-        // attendo tutte le richieste completate
-        Promise.all(requests)
-            .then(responses => {
-                responses.forEach(response => {
-                    this.mails.push(response.data.response);
-                });
-            })
-    }
-}).mount('#app');
+         // attendo tutte le richieste completate
+         Promise.all(requests)
+             .then(responses => {
+                 responses.forEach(response => {
+                     this.mails.push(response.data.response);
+                 });
+             })
+     }
+ }).mount('#app');
 
+/*fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+.then((response)=> response.json())
+.then((data) => {
+    console.log(data);
+})
+*/
